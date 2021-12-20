7f03f792774f5cf1f406d1cd123022dd76de6b3c
Ahmad-Sai <85080576+Ahmad-Sai@users.noreply.github.com>
2021-12-20 15:04:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,6 +10,8 @@ class clustering:
         self.threshold = threshold
         self.weights = np.array([1,1,1])
         
+        
+    # creates 700 random data points using numpy arrays
     def createData(self):
         self.cluster1 = np.array([[.30,.15,.92]]) + np.random.normal(0, 0.05, (100,3))
         self.cluster2 = np.array([[.59,.73,.22]]) + np.random.normal(0, 0.05, (100,3))
@@ -20,9 +22,11 @@ class clustering:
         self.cluster7 = np.array([[.66,.62,.55]]) + np.random.normal(0, 0.05, (100,3))
 
 
-        self.result = np.concatenate((self.cluster1, self.cluster2, self.cluster3, self.cluster4, self.cluster5, self.cluster6, self.cluster7))
-        self.normalized = self.result/np.max(self.result,axis=0) # normalizes data
+        self.result = np.concatenate((self.cluster1, self.cluster2, self.cluster3, self.cluster4, self.cluster5, self.cluster6, self.cluster7))  # puts all of the data points into 1 numpy array
+        self.normalized = self.result/np.max(self.result,axis=0)      # normalizes data
 
+        
+    # plots the unclustered data onto a 3D graph    
     def plotOriginal(self):
         fig = plt.figure(figsize=(6, 6), dpi=100)
         ax = fig.add_subplot(projection='3d')
@@ -37,34 +41,38 @@ class clustering:
 
         plt.show()
         
-
+        
+        
+    # chooses 7 unqiue and random intial points to begin comparisons
     def randoom(self):
         index = np.random.choice(len(self.normalized), self.k, replace=False)
         self.representatives = self.normalized[index]        
     
+    # finds the distance betweeen 2 points and calculates the dot product using weights 
     def distance(self, pointA, pointB, weights):
         diff = np.abs(pointA-pointB)
         return diff@weights
 
+    # assigns the data points to 7 classes based on calculated distances
     def assignClass(self):
         assignment = [-1]*len(self.normalized)
         for j in range(len(self.normalized)):
             closestDist = 9999999
             closestClassIndex = 0
             for i in range(len(self.representatives)):
-                carDist = self.distance(self.representatives[i], self.normalized[j], self.weights)
-                if carDist < closestDist:
-                    closestDist = carDist
+                dist = self.distance(self.representatives[i], self.normalized[j], self.weights)
+                if dist < closestDist:
+                    closestDist = dist
                     closestClassIndex = i
             assignment[j] = closestClassIndex
 
         return assignment
     
-    
+    # makes the classes array
     def makeClasses(self):
         self.classes = self.assignClass()
     
-    
+    # calculates new represnatives of which new distances will be calculated from
     def newReprenstatives(self):
         newReps = self.representatives[:]
         for i in range(self.k):
@@ -79,7 +87,7 @@ class clustering:
                 newReps[i] = classTotal/countClass
         return newReps
 
-    
+    # optimized the clustering process until the thershold is met
     def optimize(self):
         while True:
             self.representatives = self.newReprenstatives()
@@ -87,15 +95,14 @@ class clustering:
 
             counter = np.count_nonzero(self.classes == new_classes)
             if counter>0:
-                if counter/len(self.classes) < self.threshold:
+                if counter/len(self.classes) < self.threshold:  # stops looping when threshold is met
                     break
 
             self.classes = new_classes
             
-        
+    # puts clustered data into 7 different classes (arrays)
     def sortClasses(self):
         self.clusters = [[] for i in range(self.k)]
-
 
         for j in range(self.k):
             for i in range(len(self.normalized)):
@@ -103,6 +110,7 @@ class clustering:
                     self.clusters[j].append(self.normalized[i])
 
         
+    # plots the clustered data onto a 3D graph
     def plotNew(self):
         class1 = np.array(self.clusters[0])
         class2 = np.array(self.clusters[1])
@@ -114,6 +122,7 @@ class clustering:
         
         fig = plt.figure(figsize=(6, 6), dpi=100)
         bx = fig.add_subplot(projection='3d')
+        
         bx.scatter(class1[:,0], class1[:,1], class1[:,2])
         bx.scatter(class2[:,0], class2[:,1], class2[:,2])
         bx.scatter(class3[:,0], class3[:,1], class3[:,2])
@@ -123,6 +132,7 @@ class clustering:
         bx.scatter(class7[:,0], class7[:,1], class7[:,2])
         plt.show()
 
+    # calculates the standard deviation of the clustered and unclustered data points
     def getStandDev(self):
         sumClasses = 0
 
@@ -130,17 +140,4 @@ class clustering:
             sumClasses += self.distance(self.result[i],self.representatives[self.classes[i]],self.weights)
 
 
-        return sumClasses/len(self.result)
-        
-        
-        
-cluster1 = clustering(7, 0.1)
-cluster1.createData()
-cluster1.plotOriginal()
-cluster1.randoom()
-cluster1.assignClass()
-cluster1.makeClasses()
-cluster1.optimize()
-cluster1.sortClasses()
-cluster1.plotNew()
-print(cluster1.getStandDev())
+        return (sumClasses/len(self.result), math.sqrt(0.05))
